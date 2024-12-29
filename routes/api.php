cbddf1088e75ed6f87c9ab4cac04d5ccbeed5f65
The-BoxHead-Guy <astralgmd@gmail.com>
2024-12-29 09:30:37 -0400

    add CRUD routes for students in API

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,26 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", function () {
-    return response()->json(["message" => "Hello World!"], 200);
-});
-
 Route::get("/students", function () {
     return response()->json(["message" => "Getting Students List..."], 200);
+});
+
+Route::get("/student/{id}", function () {
+    return response()->json(["message" => "Getting Student"], 200);
+});
+
+Route::post("/students", function () {
+    return response()->json(["message" => "Creating the student"], 200);
+});
+
+Route::put("/student/{id}", function () {
+    return response()->json(["message" => "Updating student"], 200);
+});
+
+Route::patch("/student/{id}/property/{name}", function () {
+    return response()->json(["message" => "changing attribute of student {id}"], 200);
+});
+
+Route::delete("/student/{id}", function () {
+    return response()->json(["message" => "Deleting student..."], 200);
 });
